1e47c5e70250f4f61c61dee534f985ab47a7ffb7
MobixMAC04 <mobixmac04@MobixMAC04s-iMac.local>
2019-10-17 18:23:35 +0700

    create project and print table

---
+++ createPdf.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+// Build paths
+const { buildPathHtml, buildPathPdf } = require('./buildPaths');
+
+const printPdf = async () => {
+	console.log('Starting: Generating PDF Process, Kindly wait ..');
+	/** Launch a headleass browser */
+    const browser = await puppeteer.launch();
+    
+	/* 1- Ccreate a newPage() object. It is created in default browser context. */
+	const page = await browser.newPage();
+    console.log('---browser to newpage ok ----', buildPathHtml);
+    /* 2- Will open our generated `.html` file in the new Page instance. */
+	await page.goto('file:' + buildPathHtml, { waitUntil: 'networkidle0' });
+    console.log('---page goto ok----');
+	/* 3- Take a snapshot of the PDF */
+	const pdf = await page.pdf({
+		format: 'A4',
+		margin: {
+			top: '20px',
+			right: '20px',
+			bottom: '20px',
+			left: '20px'
+		}
+	});
+	/* 4- Cleanup: close browser. */
+	await browser.close();
+	console.log('Ending: Generating PDF Process');
+	return pdf;
+};
+
+const init = async () => {
+	try {
+		const pdf = await printPdf();
+		fs.writeFileSync(buildPathPdf, pdf);
+		console.log('Succesfully created an PDF table');
+	} catch (error) {
+		console.log('Error generating PDF', error);
+	}
+};
+
+init();
